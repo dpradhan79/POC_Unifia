4c551e71a35898f9ea33b6f8e784eddc50f93458
Rupesh Mishra <E002129@cigniti.com>
2018-05-28 18:25:44 +0530

    Updated code till Physician

--- Unifia_Admin_Page/src/poc/cigniti/multiscope/procedure/MultiScopeProcedureImpl.java
+++ Unifia_Admin_Page/src/poc/cigniti/multiscope/procedure/MultiScopeProcedureImpl.java
@@ -40,6 +40,26 @@ public class MultiScopeProcedureImpl extends ExecutionContext {
 	public void e_scanStaff1() {
 		this.scannerPage.scanItem("Procedure Room 1", "Staff", "F1_FN_001 F1_LN_001(F1001)");
 	}
+	
+	@Edge
+	public void e_scanScope2() {
+		this.scannerPage.scanItem("Procedure Room 1", "Scope", "QVLT0002");
+	}
+	
+	@Edge
+	public void e_scanStaff2() {
+		this.scannerPage.scanItem("Procedure Room 1", "Staff", "F1_FN_002 F1_LN_002(F1002)");
+	}
+	
+	@Edge
+	public void e_scanPatient() {
+		this.scannerPage.scanItem("Procedure Room 1", "Patient", "F1X000001");
+	}
+	
+	@Edge
+	public void e_scanPhysician() {
+		//this.scannerPage.scanItem("Procedure Room 1", "Staff", "Physician", "F1_DR_001 F1_PHYS_001(F1Dr001)");
+	}
 
 	@Vertex
 	public void v_RoomAvailable() {
@@ -55,7 +75,7 @@ public class MultiScopeProcedureImpl extends ExecutionContext {
 	}
 
 	@Vertex
-	public void v_scope1() {
+	public void v_Scope1() {
 		// Scanner Validation
 		this.scannerPage.isScannerResponseValid("Hang time");
 		this.scannerPage.isScannerResponseValid("days");
@@ -71,8 +91,64 @@ public class MultiScopeProcedureImpl extends ExecutionContext {
 	public void v_Staff1() {
 		
 		// Scanner Validation
+		this.scannerPage.isScannerResponseValid("Staff");
+		this.scannerPage.isScannerResponseValid("Scanned");
+		this.scannerPage.isScannerCountValid(3);
+		
+		// TODO - DB Validation
+		
+	}
+	
+	@Vertex
+	public void v_Scope2() {
+		// Scanner Validation
+		this.scannerPage.isScannerResponseValid("Hang time");
+		this.scannerPage.isScannerResponseValid("days");
+		this.scannerPage.isScannerCountValid(4);
+
+		// Dashboard Validation
+		this.unifiaPage.isProcedureRoomUpdated("Procedure Room 1", "QVLT0002");
+
+		// TODO - DB Validation
+	}
+	
+	@Vertex
+	public void v_Staff2() {
+		
+		// Scanner Validation
+		this.scannerPage.isScannerResponseValid("Staff");
+		this.scannerPage.isScannerResponseValid("Scanned");
+		this.scannerPage.isScannerCountValid(5);
+		
+		// TODO - DB Validation
+		
+	}
+	
+	@Vertex
+	public void v_Patient() {
+		
+		// Scanner Validation
+		this.scannerPage.isScannerResponseValid("Patient");
+		this.scannerPage.isScannerResponseValid("Scanned");
+		this.scannerPage.isScannerCountValid(6);
 		
 		// Dashboard Validation
+		this.unifiaPage.isProcedureRoomUpdated("Procedure Room 1", "In Use");
+		
+		// TODO - DB Validation
+		
+	}
+	
+	@Vertex
+	public void v_Physician() {
+		
+		// Scanner Validation
+		this.scannerPage.isScannerResponseValid("Physician");
+		this.scannerPage.isScannerResponseValid("Scanned");
+		this.scannerPage.isScannerCountValid(7);
+		
+		// Dashboard Validation
+		this.unifiaPage.isProcedureRoomUpdated("Procedure Room 1", "In Use");
 		
 		// TODO - DB Validation
 		
